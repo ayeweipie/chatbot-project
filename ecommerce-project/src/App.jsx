5dcd09b958cf7403ec92879010b6e044f1f65f81
ayeweipie <rocerojimmy.2024@gmail.com>
2026-08-17 11:22:24 +0800

    generate the html for the orders page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -11,8 +11,7 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/cart-items?expand=product")
-    .then((response) => {
+    axios.get("/api/cart-items?expand=product").then((response) => {
       setCart(response.data);
     });
   }, []);
@@ -21,7 +20,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="orders" element={<OrdersPage />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
